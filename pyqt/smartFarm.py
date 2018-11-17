a4ce358049ed36dbcea73e012c445d8c0123396d
itsmysurport <itsmysurport@gmail.com>
2018-11-17 14:37:24 +0900

    ADD LED Dimming

--- pyqt/smartFarm.py
+++ pyqt/smartFarm.py
@@ -1,5 +1,13 @@
+import serial
 from PyQt5 import QtCore, QtGui, QtWidgets
 from smartFarm_gui import *
+
+ser = serial.Serial(port='COM5',
+                    baudrate=115200,
+                    parity=serial.PARITY_NONE,
+                    stopbits=serial.STOPBITS_ONE,
+                    bytesize=serial.EIGHTBITS,
+                    timeout=1)
 
 red_status = 1
 green_status = 1
@@ -16,52 +24,106 @@ def event(self):
     self.cameraMenu.clicked.connect(lambda x:self.stackedWidget.setCurrentIndex(3))
 
 def red_onoff(self):
-    global red_status
+    global red_status, green_status, white_status
+    red = '000'
+    green = '000'
+    white = '000'
     print('Clicked')
+
+    # Control Device
+    if(green_status):   green='255'
+    if(white_status):   white='255'
+
     if(red_status):
+        # Control GUI CODE
         self.redButton.setStyleSheet("background-image: url(:/icon/images_backup/117x46_off_button.png);\n"
         "border: none;")
         self.red_bulb.setStyleSheet("background-image: url(:/icon/images_backup/67x81_off_bulb.png);\n")
+
         # LED OFF CODE
         red_status=0
     else:
+        # Control GUI CODE
+        print('OFF')
         self.redButton.setStyleSheet("background-image: url(:/icon/images_backup/117x46_on_button.png);\n"
         "border: none;")
         self.red_bulb.setStyleSheet("background-image: url(:/icon/images_backup/67x81_red_bulb.png);\n")
+
+        red = '255'
+
         # LED ON CODE
         red_status=1
 
+    # run function
+    message = '\x02' + 'L01' + 'F000' + 'R' + red + 'W' + white + 'B' + green + '\x03\r\n'
+    ser.write(bytes(message.encode()))
+    print(message)
+
 def green_onoff(self):
-    global green_status
+    global red_status, green_status, white_status
+    red = '000'
+    green = '000'
+    white = '000'
     print('Clicked')
+
+    # Control Device
+    if(red_status):   red='255'
+    if(white_status):   white='255'
+
     if(green_status):
+        # Control GUI CODE
         self.greenButton.setStyleSheet("background-image: url(:/icon/images_backup/117x46_off_button.png);\n"
         "border: none;")
         self.green_bulb.setStyleSheet("background-image: url(:/icon/images_backup/67x81_off_bulb.png);\n")
+
         # LED OFF CODE
         green_status=0
     else:
         self.greenButton.setStyleSheet("background-image: url(:/icon/images_backup/117x46_on_button.png);\n"
         "border: none;")
         self.green_bulb.setStyleSheet("background-image: url(:/icon/images_backup/67x81_green_bulb.png);\n")
+
+        green = '255'
         # LED ON CODE
         green_status=1
 
+    # run function
+    message = '\x02' + 'L01' + 'F000' + 'R' + red + 'W' + white + 'B' + green + '\x03\r\n'
+    ser.write(bytes(message.encode()))
+    print(message)
+
 def white_onoff(self):
-    global white_status
+    global red_status, green_status, white_status
+    red = '000'
+    green = '000'
+    white = '000'
     print('Clicked')
+
+    # Control Device
+    if(red_status):   red='255'
+    if(green_status):   green='255'
+
     if(white_status):
+
         self.whiteButton.setStyleSheet("background-image: url(:/icon/images_backup/117x46_off_button.png);\n"
         "border: none;")
         self.white_bulb.setStyleSheet("background-image: url(:/icon/images_backup/67x81_off_bulb.png);\n")
+
         # LED OFF CODE
         white_status=0
     else:
         self.whiteButton.setStyleSheet("background-image: url(:/icon/images_backup/117x46_on_button.png);\n"
         "border: none;")
         self.white_bulb.setStyleSheet("background-image: url(:/icon/images_backup/67x81_white_bulb.png);\n")
+
+        white = '255'
         # LED ON CODE
         white_status=1
+
+    # run function
+    message = '\x02' + 'L01' + 'F000' + 'R' + red + 'W' + white + 'B' + green + '\x03\r\n'
+    ser.write(bytes(message.encode()))
+    print(message)
 
 Ui_MainWindow.event = event
 Ui_MainWindow.red_onoff = red_onoff
@@ -76,6 +138,7 @@ if __name__=="__main__":
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     ui.event()
-    MainWindow.showFullScreen()
+    ser.write(bytes('\x02L01F000R255W255B255\x03\r\n'.encode()))    # Device __init__
+    MainWindow.show()
 
     sys.exit(app.exec_())
